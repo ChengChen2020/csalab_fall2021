7d8656b447ce1315a0fcd6beb9321688cbfdd227
Cheng-Chen-NYU <cc6858@nyu.edu>
2021-12-01 20:57:15 -0500

    finish lab4

--- lab4/branchsimulator_skeleton.cpp
+++ lab4/branchsimulator_skeleton.cpp
@@ -5,31 +5,88 @@
 
 using namespace std;
 
+class PHT {
+private:
+    int size;
+    vector<vector<int> > counter;
+
+public:
+    PHT(int m) {
+        size = pow(2, m);
+        counter.assign(size, vector<int> (2, 1));
+    }
+
+    int predict(bitset<32> pc) {
+        return counter[pc.to_ulong() % size][0];
+    }
+
+    bool correct_update(bitset<32> pc, bool taken) {
+        int index = pc.to_ulong() % size;
+        if (taken) {
+            if (counter[index][0] == 1) {
+                counter[index][1] = 1;
+                return true;
+            } else {
+                if (counter[index][1] == 1)
+                    counter[index].assign({1, 0});
+                else
+                    counter[index].assign({0, 1});
+                return false;
+            }
+        } else {
+            if (counter[index][0] == 0) {
+                counter[index][1] = 0;
+                return true;
+            } else {
+                if (counter[index][1] == 0)
+                    counter[index].assign({0, 1});
+                else
+                    counter[index].assign({1, 0});
+                return false;
+            }
+        }
+    }
+};
+
 int main (int argc, char** argv) {
-	ifstream config;
-	config.open(argv[1]);
+    ifstream config;
+    config.open(argv[1]);
 
-	int m;
-	config >> m;
- 
-	config.close();
+    int m;
+    config >> m;
 
-	ofstream out;
-	string out_file_name = string(argv[2]) + ".out";
-	out.open(out_file_name.c_str());
-	
-	ifstream trace;
-	trace.open(argv[2]);
+    PHT pht(m);
 
+    config.close();
 
-	while (!trace.eof()) {
-		unsigned long pc; bool taken;
-		trace >> std::hex >> pc >> taken;
-		bool prediction;
-		prediction = true;
-		out << prediction;
-	}
-	 
-	trace.close();	
-	out.close();
+    ofstream out;
+    string out_file_name = string(argv[2]) + ".out";
+    out.open(out_file_name.c_str());
+
+    ifstream trace;
+    trace.open(argv[2]);
+
+    int total_wrong = 0;
+    int total = 0;
+
+    unsigned long addr;
+    bool taken;
+
+    while (trace >> std::hex >> addr >> taken) {
+        bitset<32> pc = bitset<32> (addr);
+        int prediction = pht.predict(pc);
+        if (!pht.correct_update(addr, taken)) {
+            total_wrong += 1;
+        }
+        total += 1;
+        out << prediction << endl;
+    }
+
+    float miss_rate = 0;
+    miss_rate = (float) total_wrong / (float) total;
+    cout << "m = " << m << endl;
+    cout << "miss_rate = " << miss_rate << endl;
+    
+    trace.close();
+    out.close();
 }
