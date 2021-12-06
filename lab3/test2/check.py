042dd08341c7bdaadbbc0f021c3b40fb57dd8f65
Cheng-Chen-NYU <cc6858@nyu.edu>
2021-12-06 16:44:01 -0500

    testcase

---
+++ lab3/test2/check.py
@@ -0,0 +1,7 @@
+with open('trace.out', 'r') as test, open('trace_grading.out', 'r') as reference:
+    for line1 in test:
+        for line2 in reference:
+            if line1 != line2:
+                print(line1)
+                print(line2)
+            break
