d93c19ddcaf48e294a6bdddc42a6479402054082
Cheng-Chen-NYU <cc6858@nyu.edu>
2021-12-06 16:44:46 -0500

    done

--- lab3/test2/check.py
+++ lab3/test2/check.py
@@ -1,4 +1,4 @@
-with open('trace.out', 'r') as test, open('trace_grading.out', 'r') as reference:
+with open('trace.out', 'r') as test, open('trace_grading', 'r') as reference:
     for line1 in test:
         for line2 in reference:
             if line1 != line2:
